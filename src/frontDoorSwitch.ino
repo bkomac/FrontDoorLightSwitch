5d8ee5dfd7555fa220cf249ce4a8fbd9bb94787a
bkomac <borut@komac.si>
2018-01-13 20:26:23 +0100

    set up

--- src/frontDoorSwitch.ino
+++ src/frontDoorSwitch.ino
@@ -4,17 +4,16 @@ Espiot espiot;
 
 // Vcc measurement
 //ADC_MODE(ADC_VCC);
-int lightTreshold = 0; // 0 - dark, >100 - light
 
 String sensorData = "";
 int sensorValue;
 
 // CONF
-String ver = "1.0.0";
+String ver = "1.0.2";
 long lastTime = millis();
 
 // PIR
-#define PIRPIN 12
+#define PIRPIN 14
 
 #define BUILTINLED 2
 #define RELEY 13
@@ -29,6 +28,7 @@ void setup() { //------------------------------------------------
   pinMode(RELEY, OUTPUT);
   pinMode(BUILTINLED, OUTPUT);
 
+  stateOff("AUTO");
 
   Serial.println("FrontDoorSwitch ...v" + String(ver));
   delay(300);
@@ -57,14 +57,69 @@ void setup() { //------------------------------------------------
     root["id"] = espiot.getDeviceId();
     root["name"] = espiot.deviceName;
     root["mode"] = MODE;
-    if(digitalRead(PIRPIN) == HIGH)
-        root["state"] = "on";
-    else
-        root["state"] = "off";
+    root["state"] = getState();
 
     root["light"] = sensorValue;
 
-    JsonObject &rgb = root.createNestedObject("rgb");
+    root.printTo(Serial);
+
+    String content;
+    root.printTo(content);
+    espiot.server.send(200, "application/json", content);
+  });
+
+  espiot.server.on("/switch/on", HTTP_GET, []() {
+    espiot.blink();
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+
+    stateOn("MANUAL");
+
+    root["id"] = espiot.getDeviceId();
+    root["name"] = espiot.deviceName;
+    root["mode"] = MODE;
+    root["state"] = getState();
+    root["light"] = sensorValue;
+
+    root.printTo(Serial);
+
+    String content;
+    root.printTo(content);
+    espiot.server.send(200, "application/json", content);
+  });
+
+  espiot.server.on("/switch/off", HTTP_GET, []() {
+    espiot.blink();
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+
+    stateOff("MANUAL");
+
+    root["id"] = espiot.getDeviceId();
+    root["name"] = espiot.deviceName;
+    root["mode"] = MODE;
+    root["state"] = getState();
+    root["light"] = sensorValue;
+
+    root.printTo(Serial);
+
+    String content;
+    root.printTo(content);
+    espiot.server.send(200, "application/json", content);
+  });
+
+  espiot.server.on("/switch/auto", HTTP_GET, []() {
+    espiot.blink();
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+
+    stateAuto();
+
+    root["id"] = espiot.getDeviceId();
+    root["name"] = espiot.deviceName;
+    root["mode"] = MODE;
+    root["state"] = getState();
+    root["light"] = sensorValue;
 
     root.printTo(Serial);
 
@@ -80,55 +135,85 @@ void setup() { //------------------------------------------------
 // -----------------------------------------------------------------------------
 void loop() {
   yield();
-
-  espiot.loop();
-
   sensorValue = analogRead(A0); // read analog input pin 0
   int inputState = digitalRead(PIRPIN);
-  yield();
 
   DynamicJsonBuffer jsonBuffer;
   JsonObject &root = jsonBuffer.createObject();
   root["id"] = espiot.getDeviceId();
   root["name"] = espiot.deviceName;
+  root["state"] = getState();
   root["light"] = sensorValue;
-  root["motion"] = NULL;
-
-  yield();
+  root["motion"] = false;
 
   if (MODE == "AUTO") {
     if (inputState == HIGH) {
       Serial.println(F("Sensor high..."));
-      digitalWrite(BUILTINLED, LOW);
-      digitalWrite(RELEY, HIGH);
+
+      if(sensorValue < espiot.lightThreshold){
+          stateOn("AUTO");
+      }else{
+          Serial.println("Trashold preventing on state..." + String(espiot.lightThreshold));
+      }
 
       lastTime = millis();
-      if(!sentMsg){
-        String content;
-        root["motion"] = true;
-        root.printTo(content);
-        espiot.mqPublishSubTopic(content, "motion");
-        sentMsg = true;
-      }
     } else{
         root["motion"] = false;
     }
   }
 
-  if (millis() > lastTime + espiot.timeOut && inputState != HIGH) {
-    digitalWrite(RELEY, LOW);
-    digitalWrite(BUILTINLED, HIGH);
+  yield();
 
-    sentMsg = false;
+  if (inputState == HIGH && !sentMsg) {
+      String content;
+      root["motion"] = true;
+      root.printTo(content);
+      espiot.mqPublishSubTopic(content, "motion");
+      sentMsg = true;
+  }
+
+  if (millis() > lastTime + espiot.timeOut && inputState != HIGH) {
+    stateOff("AUTO");
+
     Serial.println("timeout... " + String(lastTime));
     Serial.print("Light: ");
     Serial.println(sensorValue, DEC);
 
     String content;
     root.printTo(content);
-    espiot.mqPublish(content);
+    if (!sentMsg){
+        espiot.mqPublish(content);
+    }
 
+    sentMsg = false;
     lastTime = millis();
   }
-  delay(200);
+  yield();
+  espiot.loop();
+  delay(100);
+
 } //---------------------------------------------------------------
+
+void stateOn(String mode){
+    Serial.println(F("\nSwitch on ..."));
+    digitalWrite(BUILTINLED, LOW);
+    digitalWrite(RELEY, HIGH);
+    MODE = mode;
+}
+void stateOff(String mode){
+    Serial.println(F("\nSwitch off ..."));
+    digitalWrite(BUILTINLED, HIGH);
+    digitalWrite(RELEY, LOW);
+    MODE = mode;
+}
+void stateAuto(){
+    Serial.println(F("\nSwitch to auto ..."));
+    MODE = "AUTO";
+}
+
+String getState(){
+    if(digitalRead(RELEY) == HIGH)
+        return "on";
+    else
+        return "off";
+}
